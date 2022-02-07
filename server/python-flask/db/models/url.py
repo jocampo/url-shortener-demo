00ac2c3cae56f11883c6cce32c079814417c8409
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2022-02-07 00:35:50 -0500

    repr for url model

--- server/python-flask/db/models/url.py
+++ server/python-flask/db/models/url.py
@@ -6,3 +6,6 @@ class Url(BaseModel):
     short_url = db.Column("short_url", db.String, nullable=False)
     long_url = db.Column("long_url", db.String, nullable=False)
     hits = db.Column("hits", db.BigInteger, nullable=False, default=0)
+
+    def __repr__(self):
+        return f"<URL Model: {self.short_url} <-> {self.long_url}>"
