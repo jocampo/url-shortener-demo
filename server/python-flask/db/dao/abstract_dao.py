165cb0c69a029f2009b2c4572b2ff94eb1f16a77
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2022-02-08 01:20:13 -0500

    first rough pass at dao, absolutely not tested :^)

---
+++ server/python-flask/db/dao/abstract_dao.py
@@ -0,0 +1,61 @@
+from abc import ABC
+
+from db.models.base_model import BaseModel
+from app.create_app import db
+
+
+class AbstractDAO(ABC):
+    """
+    Base class for DAOs. Needs to be extended for more complex handling of objects in the DB.
+    The idea is to NOT have to pass around/import the db object everywhere.
+    """
+
+    @staticmethod
+    def create(entity):
+        """
+        Generic implementation to create a given entity. Useful for creates/updates
+        :param entity: SQLAlchemy entity
+        """
+        assert entity
+        db.session.add(entity)
+        db.session.commit()
+
+    @staticmethod
+    def delete(entity: BaseModel):
+        """
+        Generic implementation for the delete operation of a given entity
+        :param entity: SQLAlchemy entity
+        """
+        assert entity
+        assert entity.id > 0
+        db.session.delete(entity)
+        db.session.commit()
+
+    @staticmethod
+    def get(entity: BaseModel):
+        """
+        Generic implementation to fetch a single given entity with a matching id
+        :param entity: SQLAlchemy entity with a valid id
+        :return matching SQLAlchemy entity from the database
+        """
+        assert entity.id > 0
+        return (db.session.connection
+                .query(entity.__class__)
+                .filter(entity.__class__.id == entity.id)
+                .one())
+
+    @staticmethod
+    def begin():
+        db.session.begin()
+
+    @staticmethod
+    def rollback():
+        db.session.rollback()
+
+    @staticmethod
+    def commit():
+        db.session.commit()
+
+    @staticmethod
+    def flush():
+        db.session.flush()
